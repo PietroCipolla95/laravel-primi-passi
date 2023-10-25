e6189c1b322c4490059423234db16ce42d7de995
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-10-25 15:56:10 +0200

    my new homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Step</title>
+</head>
+
+<body>
+
+    <h1>
+        {{$title}}
+    </h1>
+
+    <h5>
+        {{$sub_title}}
+    </h5>
+
+    <p>
+        {{$message}}
+    </p>
+
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'title' => 'Hello World ✌',
+        'sub_title' => 'brought by Laravel 😁',
+        'message' => 'My first application with Laravel 🙌',
+    ];
+
+
+    return view('homepage', $data);
 });
